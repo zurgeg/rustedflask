8a6c76069072e20f3bcc0367df4753d56da908bb
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-12-18 22:20:57 -0500

    fully impl examples

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use rustedflask::{
     flask::App,
     jinja::render_template,
@@ -12,9 +14,20 @@ fn main_route(request: HTTPRequest) -> HTTPResponse {
 }
 
 fn template_route(request: HTTPRequest) -> HTTPResponse {
-    render_template(file, variables)
+    let template_name = "template.html.jinja2";
+    let mut variables = HashMap::new();
+    variables.insert("template_name", template_name.to_string());
+    match render_template(template_name, variables) {
+        Ok(content) => HTTPResponse::from(&*content),
+        Err(why) => todo!(),
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new("example".to_string());
+    app.route("/", main_route);
+    app.route("/template", template_route);
+
+    app.run("0.0.0.0:5000");
+    panic!("Couldn't run");
 }
