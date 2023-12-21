b36e71467ec6822a7e75038707fef8508b8a9a79
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-12-21 12:23:46 -0500

    Better regex for inheritance

--- src/jinja/consts.rs
+++ src/jinja/consts.rs
@@ -4,6 +4,4 @@ pub const INCLUDE: &str = r#"\{% include "(?P<filename>.*)" %\}"#;
 
 pub const EXTEND: &str = r#"\{% extend "(?P<filename>.*)" %\}"#;
 
-pub const BLOCK_START: &str = r"\{% block (?P<blockname>.*) %\}";
-
-pub const BLOCK_END: &str = r#"\{% endblock %\}"#;
+pub const BLOCK: &str = r"(?ms)\{% block (?P<blockname>.*) %\}\n?(?P<blockcontent>.*)\n?\{% endblock %\}";
