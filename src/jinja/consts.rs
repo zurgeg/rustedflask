9974366ada5915cd4947ea78d1fa7ede17dacd3d
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-12-21 12:20:28 -0500

    Add regex for inheritance

--- src/jinja/consts.rs
+++ src/jinja/consts.rs
@@ -1,3 +1,9 @@
 pub const REPLACE: &str = r#"\{\{ (?P<variable>.*) \}\}"#;
 
 pub const INCLUDE: &str = r#"\{% include "(?P<filename>.*)" %\}"#;
+
+pub const EXTEND: &str = r#"\{% extend "(?P<filename>.*)" %\}"#;
+
+pub const BLOCK_START: &str = r"\{% block (?P<blockname>.*) %\}";
+
+pub const BLOCK_END: &str = r#"\{% endblock %\}"#;
