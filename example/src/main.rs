dd7e871f6e2fd40cf624647601e6ad5838832bf8
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-12-21 09:57:59 -0500

    add example for other methods

--- example/src/main.rs
+++ example/src/main.rs
@@ -26,10 +26,15 @@ fn template_route(_request: HTTPRequest) -> HTTPResponse {
     }
 }
 
+fn route_you_can_only_post_to(_request: HTTPRequest) -> HTTPResponse {
+    "You can only use the POST method to access this route".into()
+}
+
 fn main() {
     let mut app = App::new("example".to_string());
     app.route("/", main_route);
     app.route("/template", template_route);
+    app.route_with_allowed_methods("/postonly", route_you_can_only_post_to, vec!["POST".to_string()]);
 
     app.run("0.0.0.0:5000");
     panic!("Couldn't run");
